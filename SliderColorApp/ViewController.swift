c8a2e3c2ad1617f2b92e7c553f016e62fa4748a9
Ula <1295593@mail.ru>
2022-01-18 14:09:41 +0800

    raw HW

--- SliderColorApp/ViewController.swift
+++ SliderColorApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redNumber: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenNumber: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueNumber: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColor.layer.cornerRadius = 10
     }
+    
 
-
+    @IBAction func redSliderMove() {
+        redNumber.text = String(format: "%.2f", redSlider.value)
+        changeColorView()
+    }
+    
+    @IBAction func greenSliderMove() {
+        greenNumber.text = String(format: "%.2f", greenSlider.value)
+        changeColorView()
+    }
+    
+    @IBAction func blueSliderMove() {
+        blueNumber.text = String(format: "%.2f", blueSlider.value)
+        changeColorView()
+    }
+    
+    private func changeColorView() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+    viewColor.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+}
 }
 
